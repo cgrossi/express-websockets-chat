ba9759ebe4ecb09381e5bca8891eba399d8c8bc6
cgrossi <cgrossi@gmail.com>
2019-07-19 17:18:31 -0600

    Add some style and functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,67 @@
-io()
+const socket = io()
+
+// Elements
+const $messageForm = document.querySelector('form')
+const $messageFormInput = $messageForm.querySelector('form input')
+const $messageFormButton = $messageForm.querySelector('form button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+socket.on('message', (message) => {
+  console.log(message.text)
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm a')
+  })
+  $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (message) => {
+  console.log(message)
+  const html = Mustache.render(locationMessageTemplate, {
+    url: message.url,
+    createdAt: moment(message.createdAt).format('h:mm a')
+  })
+  $messages.insertAdjacentHTML('beforeend', html)
+})
+
+$messageForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  $messageFormButton.setAttribute('disabled', 'disabled')
+  const message = e.target.elements.message.value
+  socket.emit('sendMessage', message, (error) => {
+    $messageFormButton.removeAttribute('disabled')
+    $messageFormInput.value = ''
+    $messageFormInput.focus()
+    if(error) {
+      return console.log(error)
+    }
+
+    console.log('Message delivered!')
+  })
+})
+
+$sendLocationButton.addEventListener('click', () => {
+  if(!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser')
+  }
+  
+  $sendLocationButton.setAttribute('disabled', 'disabled')
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {longitude: position.coords.longitude, latitude: position.coords.latitude}, () => {
+      $sendLocationButton.removeAttribute('disabled')
+      console.log('Location sent!')
+    })
+  })
+})
+
+socket.emit('join', {username, room}, (error) => {
+  
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const http = require('http')
 const app = express()
 const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const port = process.env.PORT || 3000
 const server = http.createServer(app)
@@ -14,8 +17,45 @@ const io = socketio(server)
 // then pass variable name into the command below
 app.use(express.static('public'))
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
   console.log('New web socket connection')
+
+  socket.on('join', ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room })
+
+    if(error) {
+      return callback(error)
+    }
+    socket.join(user.room)
+
+    socket.emit('message', generateMessage('Welcome!'))
+
+    socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined the chatroom.`))
+
+    callback()
+  })
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id)
+
+    if(user) {
+      io.to(user.room).emit('message', generateMessage(`${user.username} has left the chatroom.`))
+    }
+  })
+
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter()
+    if(filter.isProfane(message)) {
+      return callback('Profanity is not allowed')
+    }
+    io.to('hello').emit('message', generateMessage(message))
+    callback()
+  })
+
+  socket.on('sendLocation', (position, cb) => {
+    io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${position.latitude},${position.longitude}`))
+    cb()
+  })
 })
 
 app.get('/', (req, res) => {
